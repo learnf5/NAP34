54487d81885a6826c3f5d02df18f607a0a343079
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-04-18 06:20:56 -0700

    Create NAP34-L05L01-LOGGING.sh

---
+++ NAP34-L05L01-LOGGING.sh
@@ -0,0 +1,16 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+# update lab environment
+sudo ssh nginx test -e /etc/nginx/conf.d/default.conf && sudo ssh nginx rm /etc/nginx/conf.d/default.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/LOGGING/nginx.conf
+sudo scp /tmp/nginx.conf nginx:/etc/nginx/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/INTRO/nap.conf
+sudo scp /tmp/nap.conf   nginx:/etc/nginx/conf.d/ 
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTP/{nginx,nap}.orig
+sudo scp /tmp/nginx.orig nginx:/etc/nginx/
+sudo scp /tmp/nap.orig   nginx:/etc/nginx/conf.d/
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/INTRO/hosts
+sudo scp /tmp/hosts nginx:/etc/hosts
