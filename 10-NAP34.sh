2bae6bae7c4062fb48ae3fe31cab666172dd96ff
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-04-23 10:30:25 -0400

    Update and rename common-tasks.sh to 10-NAP34.sh

--- 10-NAP34.sh
+++ 10-NAP34.sh
@@ -1,11 +1,6 @@
-# enable debugging
-set -x
-PS4='+$(date +"%T.%3N"): '
-
 # get lab-info.md for student guide bravais id
-#SHOULD THIS NOW BE $COURSE_ID??? instead of devnap?
-curl --silent https://raw.githubusercontent.com/learnf5/devnap/main/lab-info.md --output /tmp/lab-info.md
-brav_id=$(awk -F '|' "/$LAB_ID/"' {print $2}' /tmp/lab-info.md)
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/lab-info.md --output /tmp/lab-info.md
+###brav_id=$(awk -F '|' "/$LAB_ID/"' {print $2}' /tmp/lab-info.md)
 
 # install nginx license
 # ALL licenses from NGINX One (Updated to include NAP DoS repo permission)
@@ -16,11 +11,3 @@ set -x
 until sudo scp /tmp/nginx-one-eval.crt nginx:/etc/ssl/nginx/nginx-repo.crt || (( count++ > 5 )); do sleep 5; done
 until sudo scp /tmp/nginx-one-eval.key nginx:/etc/ssl/nginx/nginx-repo.key || (( count++ > 5 )); do sleep 5; done
 until sudo scp /tmp/nginx-one-eval.jwt nginx:/etc/nginx/license.jwt || (( count++ > 5 )); do sleep 5; done
-
-# run this lab's specific tasks saved on GitHub
-curl --silent --output /tmp/$LAB_ID.sh https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$LAB_ID.sh
-bash -x /tmp/$LAB_ID.sh
-
-# restart NGINX
-sudo ssh nginx systemctl stop nginx
-sudo ssh nginx systemctl start nginx
