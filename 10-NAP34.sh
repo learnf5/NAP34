0e2a20b3dee7a4496fc4f1aa153e523c306857a4
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-06-23 14:26:21 -0700

    Update 10-NAP34.sh

--- 10-NAP34.sh
+++ 10-NAP34.sh
@@ -1,3 +1,9 @@
+# Sorry for my mistake (Vicki June 21, 2025) - I left nap.conf and nap.orig on the NGINX base VM
+# So I'll remove it for all NGINX labs from this script
+# The MV NAP class labs do require the nap.conf file but Labs1-3 don't start with lab 4 nap.conf which is what is on the base VM
+# so remove nap.conf and nap.orig from base image and then in the actual lab script 20-NAP34-xxx the appropriate file will be loaded
+sudo ssh nginx sudo rm /etc/nginx/conf.d/nap.{conf,orig}
+
 # install 30 day nginx one trial licenses from Salesforce - EXPIRES May 22, 2025
 set +x
 curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token $TOKEN" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/nginx-one-eval.{crt,key,jwt}
