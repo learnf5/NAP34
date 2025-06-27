f220dd9686f064ff7893fa2fc822fd06a1df4444
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-06-27 10:47:15 -0700

    Update 20-NAP34-L09L01-LEAKAGE.sh

--- 20-NAP34-L09L01-LEAKAGE.sh
+++ 20-NAP34-L09L01-LEAKAGE.sh
@@ -3,7 +3,7 @@ sudo ssh nginx rm /etc/nginx/conf.d/default.conf
 sudo ssh nginx mkdir --parents /etc/app_protect/conf/nap.d
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/LEAKAGE/nginx.conf
 sudo scp /tmp/nginx.conf                                    nginx:/etc/nginx/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/ATTACK/nap.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/LEAKAGE/nap.conf
 sudo scp /tmp/nap.conf                                      nginx:/etc/nginx/conf.d/
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HTTP/{nginx,nap}.orig
 sudo scp /tmp/nginx.orig                                    nginx:/etc/nginx/
